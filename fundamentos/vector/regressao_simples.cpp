bfe403c57ffbfb7b02233b137642b902be6853db
Érick Alves <onemylinux@gmail.com>
2023-10-29 15:25:17 -0300

    Refatoração (vetores/): Ficou mais rápido e legível

--- fundamentos/vector/regressao_simples.cpp
+++ fundamentos/vector/regressao_simples.cpp
@@ -6,95 +6,60 @@
 
 using namespace std;
 
-double coeficienteA(vector<int> x, vector<int> y, int tamX, int tamY,
-					double mediaX, double mediaY) {
-	/**
-		Formula de A
+// Formulas matemáticas no fim do código
 
-			  ∑(xᵢ - ẍ)·(yᵢ - ÿ)
-	    a = ⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻
-	        	∑(xᵢ - ẍ)²
-		
-		∑ = Auto-soma -> somatória dos termos
-		xᵢ = valor de X quando i é igual ao termo diferente de 0, i = 1
-		ẍ = média de X
-		yᵢ = valor de Y quando i é igual ao termo diferente de 0, i = 1
-		ÿ = média de Y
-	*/
+double coeficienteA(vector<int> x, vector<int> y, int tamX, double mediaX, double mediaY) {
+	// armazenar os dominadores e denominadores de A
+	vector<int> dominadorA, denominadorA;
+	double produtos, basePotencia, A;
 
-	vector<int> dominadorA, denominadorA; // armazenar os dominadores e denominadores de 'A'
-	int autoSoma = 0;
-	double mult, baseEXP, domA, denoA, a;
-
-	// formulando Auto-soma do coeficiente angular 'A'
+	// formulando Auto-soma do coeficiente angular A
 	for (int i = 0; i < tamX; i++) {
-		mult = (x[i] - mediaX) * (y[i] - mediaY);
-		dominadorA.push_back(mult); // armazena cada multiplo como dominador de 'A'
+		// dominadores
+		produtos = (x[i] - mediaX) * (y[i] - mediaY);
+		dominadorA.push_back(produtos);
 
-		baseEXP = pow((x[i] - mediaX), 2);
-		denominadorA.push_back(baseEXP); // armazena cada base da potencia como denominador de 'A'
+		// denominadores
+		basePotencia = pow((x[i] - mediaX), 2);
+		denominadorA.push_back(basePotencia);
 	}
-	
-	// Obtem a autoSoma do dominador
-	for (int n : dominadorA) {
-		autoSoma += n;
-	}
-	domA = autoSoma;
-	autoSoma = 0;
+	produtos = 0;
+	basePotencia = 0;
 
-	// Obtem a autoSoma do denominador
-	for (int n : denominadorA) {
-		autoSoma += n;
+	for (int i = 0; i < dominadorA.size(); i++) {
+		produtos += dominadorA[i];
+		basePotencia += denominadorA[i];
 	}
-	denoA = autoSoma;
-
-	a = domA / denoA; // Fim da formula, achamos A
-	return a;
+	A = produtos / basePotencia;
+	return A;
 }
 
 double coeficienteB(double valorA, double mediaX, double mediaY) {
-	/**
-		Formula de B
-
-		b ≃ ÿ - a * ẍ
-
-		b = ponto de intersecção em y
-		ÿ = média de y
-		ẍ = média de x
-	*/
-	double b = mediaY - valorA * mediaX;
-	return b;
+	double B = mediaY - valorA * mediaX;
+	return B;
 }
 
 string formRegressao(vector<int> x, vector<int> y) {
 	// Retorna uma string como formula da reta
 
 	// Variáveis básicas Inteiras
- 	int tamX, tamY, soma, domA, denoA;
- 	tamX = x.size();
- 	tamY = y.size();
- 	soma = 0;
+ 	int tamanhoX = x.size(), tamanhoY = y.size(), somaX = 0, somaY = 0;
 
  	// Variáveis respostas de precisão flutuante
- 	double medX, medY, a, b;
+ 	double mediaX, mediaY, a, b;
 
- 	if (tamX == tamY) {
+ 	if (tamanhoX == tamanhoY) {
  		// Obtendo média de cada vector
- 		for (int num : x) {
- 			soma += num;
+ 		for (int i = 0; i < tamanhoX; i++) {
+ 			somaX += x[i];
+ 			somaY += y[i];
  		}
- 		medX = soma / tamX;
- 		soma = 0;
+ 		mediaX = somaX / tamanhoX;
+ 		mediaY = somaY / tamanhoY;
 
- 		for (int num : y) {
- 			soma += num;
- 		}
- 		medY = soma / tamY;
- 		soma = 0;
-
- 		// Pegando valores do coeficiente e arredondando em 2 casas decimal
- 		a = coeficienteA(x, y, tamX, tamY, medX, medY);
- 		b = coeficienteB(a, medX, medY);
+ 		// Obtendo os valores de A e B
+ 		a = coeficienteA(x, y, tamanhoX, mediaX, mediaY);
+ 		b = coeficienteB(a, mediaX, mediaY);
 
  		cout << endl;
  		cout << "-> Precisão de A e B" << endl;
@@ -102,32 +67,25 @@ string formRegressao(vector<int> x, vector<int> y) {
  		cout << "-> B : " << b << endl;
  		cout << endl;
 
- 		// arredondar em duas casas decimais
- 		a = round(a * 100) / 100;
-	 	b = round(b * 100) / 100;
- 		
- 		// formula da reta ŷ ≃ a * x[i] + b
- 		string formula;
- 		if (b < 0) { // sendo negativo
-	 		formula = "ŷ ≃ " + to_string(a) + " · Xᵢ - " + to_string(abs(b));
-	 		return formula;
+ 	 	string form;
+ 		// formula da reta: ŷ ≃ a · Xᵢ + b
+ 		if (b < 0) {
+	 		form = "ŷ ≃ " + to_string(a) + " · Xᵢ - " + to_string(b);
+	 		return form;
  		} else {
-	 		formula = "ŷ ≃ " + to_string(a) + " · Xᵢ + " + to_string(b);
-	 		return formula;
+	 		form = "ŷ ≃ " + to_string(a) + " · Xᵢ + " + to_string(b);
+	 		return form;
  		}
  	} else {
  		return "Passe um tamanho válido";
  	}
 }
 
-
 int main() {
-
- 	vector<int> seqX, seqY; // vectores bases
-	seqX = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
- 	seqY = {4, 5, 9, 13, 17, 21, 25, 27, 28, 30};
-
- 	string resultado = formRegressao(seqX, seqY);
+	// Vetores básico
+ 	vector<int> seqX = {1, 2, 3, 4, 5}, seqY = {3, 7, 5, 11, 14};
+ 	// resultado = 2,6 * X + 0.2
+ 	string resultado = formRegressao(seqX, seqY); 
 
  	// editando terminal
  	cout << endl;
@@ -140,3 +98,33 @@ int main() {
 
  	return 0;
 }
+/**
+  	Formula da Regressão Linear Simples
+	Ŷ ≃ A · X + B
+	
+	Y = número resposta quando variável X é i-termo
+	A = Coeficiente angular da reta
+	X = Variável a qual entra o i-termo
+	B = Ponto de intersecção Y quando X é 0
+
+	Formula de A
+
+		∑(xᵢ - ẍ)·(yᵢ - ÿ)
+    a = ⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻
+       	∑(xᵢ - ẍ)²
+	
+	∑ = Auto-soma -> somatória dos termos
+	xᵢ = Valor de X quando i é igual ao termo diferente de 0, i = 1
+	ẍ = Média de X
+	yᵢ = Valor de Y quando i é igual ao termo diferente de 0, i = 1
+	ÿ = Média de Y
+	
+
+	Formula de B
+
+	b ≃ ÿ - a · ẍ
+
+	b = Ponto de intersecção em Y
+	ÿ = Média de Y
+	ẍ = Média de X
+*/
